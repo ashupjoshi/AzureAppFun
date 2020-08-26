77819a06f616e1b64ec0bd1dcf6ff042a4739e36
ashishpjoshi11 <ashishpjoshi11@gmail.com>
2020-08-26 08:50:28 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AzureAppFun.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
